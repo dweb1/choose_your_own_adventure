903e01f3c1b8fdbacf2a36093ac571cf7a12509b
dweb1 <david.p.weber@gmail.com>
2017-07-05 14:02:01 -0400

    tried to get submit button to store first name of player

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 // var name = prompt("Hey there, gaffer. What's your first name?");
 
-$(document).ready(function() {
-    Materialize.updateTextFields();
-  });
+// $(document).ready(function() {
+//     Materialize.updateTextFields();
+//   });
 
+// function firstName() {
+// document.getElementById('input-field') onClick()
+// }
+
+var firstName = function getName() {
+	document.getElementById('input-field').value;
+}
+onclick="alert(firstName);"
