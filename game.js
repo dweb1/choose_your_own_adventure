78c9772f3fc93d4d16db6bc294d2d840f2d082fe
dweb1 <david.p.weber@gmail.com>
2017-07-06 14:00:47 -0400

    Added second level choice to game logic

--- game.js
+++ game.js
@@ -25,6 +25,22 @@ if (firstQuestion === "Africa") {
     var talent = prompt("OK, onto S America it is! We have a good relationship with a famous club in Santiago, Chile. They have two bright young talents, both defenders. The first is a centerback who is very physical but struggles with keeping his cool, while the other is a right-sided fullback with rubber-burning pace. Should we sign the CB or the RB?")
 } else if (firstQuestion === "N America") {
     var talent = prompt("We don't have the most developed relationships in N America, but let's give it a try. From my scouting contacts in Chicago, I've heard about two talents: one is a left-sided fullback with very strong crossing but deficiencies in his defensive positioning and the other is a very special support-striker who is child of a former European superstar who moved to the US before retiring from football. Who should we bring back to our club: the LB or the SS?")
-  }
+}
   
-  console.log(talent);
+if (talent === "AMF") {
+    var talentFirstChoice = prompt("Great choice, boss! We could use some more creativity up front. First order of business is determining whether we want to develop him in-house or send him out for a few years on loan. There are positives and drawbacks to either way, but it's ultimately up to you, Coach" + userName +". What do you say: loan or keep?");
+} else if (talent === "GK") {
+    var talentFirstChoice = prompt("I always say that you should build from the back, first and foremost! First order of business is determining whether we want to develop him in-house or send him out for a few years on loan. There are positives and drawbacks to either way, but it's ultimately up to you, Coach" + userName +". What'll it be: loan or keep?")
+} else if (talent === "CMF") {
+    var talentFirstChoice = prompt("A good central midfielder is the cog that makes everything tick! First order of business is determining whether we want to develop him in-house or send him out for a few years on loan. There are positives and drawbacks to either way, but it's ultimately up to you, Coach" + userName +". What do you say: loan or keep?")
+} else if (talent === "CF") {
+    var talentFirstChoice = prompt("A target-man! Bring on the firepower! First order of business is determining whether we want to develop him in-house or send him out for a few years on loan. There are positives and drawbacks to either way, but it's ultimately up to you, Mr. " + userName +". What'll it be: loan or keep?")
+} else if (talent === "CB") {
+    var talentFirstChoice = prompt("I agree, we need some steel and grit in the backline. First order of business is determining whether we want to develop him in-house or send him out for a few years on loan. There are positives and drawbacks to either way, but it's ultimately up to you, Coach " + userName +". What do you say: loan or keep?")
+} else if (talent === "RB") {
+    var talentFirstChoice = prompt("Width! Width! Width! - That's how the game is played today! First order of business is determining whether we want to develop him in-house or send him out for a few years on loan. There are positives and drawbacks to either way, but it's ultimately up to you, Coach " + userName +". What do you say: loan or keep?")
+} else if (talent === "LB") {
+    var talentFirstChoice = prompt("Width! Width! Width! - That's how the game is played today! First order of business is determining whether we want to develop him in-house or send him out for a few years on loan. There are positives and drawbacks to either way, but it's ultimately up to you, Coach " + userName +". What do you say: loan or keep?")
+} else if (talent === "SS") {
+    var talentFirstChoice = prompt("Assists and goals - this guy will do it all! First order of business is determining whether we want to develop him in-house or send him out for a few years on loan. There are positives and drawbacks to either way, but it's ultimately up to you, Coach " + userName +". What do you say: loan or keep?")
+}
